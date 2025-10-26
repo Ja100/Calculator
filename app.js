e5470aa200c8f60388f701d6aa8a79551d061763
Jallen Hunnid <aubinderulo@gmail.com>
2025-10-26 18:08:06 +0200

    Final output

--- app.js
+++ app.js
@@ -1,39 +1,134 @@
-function Add(num1,num2,operatorSign){
-    // num1 = 0;
-    // num2 = 0;
-    // num1 = parseInt(prompt('enter a number'));
-    // num2 = parseInt(prompt('enter a number'));
-    // operatorSign = prompt('enter an operator sign (+,-,*,/)');
-    // console.log(num1 + num2);
+let display = document.getElementById('display-section');
+
+const clickedDigit = document.querySelectorAll('#digitBtn');
+const clickedOperators = document.querySelectorAll('#operatorsBtn');
+const equalsBtn = document.getElementById('equalsBtn');
+const deleteBtn = document.querySelector('.deleteBtn');
+const clearBtn = document.querySelector('.clearBtn');
+
+let currentNumber =''; // always the second number
+let operator = '';
+let firstNumber = '';
+
+// Digit clicked Logic Listener
+
+clickedDigit.forEach(button =>{
+    button.addEventListener('click', (e) =>{
+        currentNumber += e.target.value;
+        display.textContent = currentNumber;        
+    })
+})
+
+
+// Operator clicked Logic Listener
+
+clickedOperators.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if (firstNumber !== '' && currentNumber !== ''){
+            const result = Operate(firstNumber, currentNumber,operator);
+            if(typeof result !== 'number'){
+                display.textContent = result;
+                firstNumber = '';
+                operator = '';
+                currentNumber = '';
+                return;
+            }
+
+            firstNumber = result.toString();
+            display.textContent = firstNumber;
+        } 
+        else if (currentNumber !== ''){
+            firstNumber = currentNumber;
+        }
+        const nxtOp = e.target.value; //when the result is infinity/NAN
+        operator = nxtOp ;
+        currentNumber ='';        
+    })
+})
+
+// Equal Button Logic Listener
+if (equalsBtn){
+    equalsBtn.addEventListener('click', () => {
+        if (firstNumber !== '' && currentNumber !== '' && operator !== ''){
+            const result = Operate(firstNumber, currentNumber, operator);
+            
+            display.textContent = result;
+            firstNumber = result.toString();
+            operator = '';
+            currentNumber = '';
+        }
+    });
 }
 
 
-function Substract(num1,num2,operatorSign){
+// Clear an entry
+function clearEntry(){
+    if(currentNumber.length > 0){
+        currentNumber = currentNumber.slice(0,-1);
+        display.textContent = currentNumber === '' ? '': currentNumber;
+    } else if(firstNumber.length > 0){
+        firstNumber  = firstNumber.slice(0,-1);
+    } else{
+        display.textContent = '0'
+    }
+}
+clearBtn.addEventListener('click',clearEntry)
+
+// Clear all 
+function clearAll(){
+    currentNumber = '';
+    operator = '';
+    firstNumber = '';
+    display.textContent = '';
+}
+deleteBtn.addEventListener('click', clearAll);
+
+
+// Arithmetic Operation Functions 
+
+function Add(num1,num2){
+    return num1 + num2;
+}
+
+function Substract(num1,num2){
     return num1 - num2;
-    
 }
 
-function Prod(num1,num2,operatorSign){
-    return num1 * num2;
-    
+function Prod(num1,num2){
+    return num1 * num2; 
 }
 
-function Quot(num1,num2,operatorSign){
+function Quot(num1,num2){
+    if(num2 === 0){
+        return "can't divide by 0"
+    }
     return num1 / num2;
     
 }
 
-function Operate() {
-    let result = 0;
-    if (operatorSign = '+'){
-        result = Add();
-    } else if(operatorSign = '-'){
-        result = Substract();
-    } else if(operatorSign = '*'){
-        result = Prod();
-    } else if(operatorSign = '/'){
-        result = Quot();
-    } else {
-        console.log("Wrong Operator!");
-    }   
+function Operate(num1Str,num2Str,operatorSign) {
+    let rslt = 0;
+    const num1 = parseFloat(num1Str);
+    const num2 = parseFloat(num2Str);
+    
+    if (operatorSign === '+'){
+        
+       rslt = Add(num1, num2, operatorSign);
+        
+    } else if(operatorSign === '-'){
+
+        rslt = Substract(num1, num2, operatorSign);
+    } else if(operatorSign === '*'){
+
+        rslt = Prod(num1, num2, operatorSign);
+    } else if(operatorSign === '/'){
+    
+        rslt = Quot(num1, num2, operatorSign);
+    } else if(typeof rslt !== 'number'){
+        return rslt
+    }
+    else {
+        console.log("Invalid Operator!");
+    }
+    return Math.round(rslt * 1000) / 1000; 
 }
